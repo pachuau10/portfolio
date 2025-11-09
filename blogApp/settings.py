ace3bd00393ea10e7d6e8e41b35583c1a2089736
pachuau10 <duhteapachuau10@gmail.com>
2025-11-09 21:05:19 +0530

    fix: Removed exposed SMTP credentials and implemented .env safety

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -162,8 +162,8 @@ else:
 # EMAIL CONFIGURATION (for contact form)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'duhteapachuau21@gmail.com'
-EMAIL_HOST_PASSWORD = 'vwvspunmiyjlbutg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
