c701bd63bfc79248bb7089733f1da27a7a8024d3
pachuau10 <duhteapachuau10@gmail.com>
2025-11-10 00:34:11 +0530

    feat: Final deployment ready commit - Integrated Neon DB and Cloudinary fixes

--- home/models.py
+++ home/models.py
@@ -1,4 +1,8 @@
+    # home/models.py
+
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class Blog(models.Model):
@@ -6,8 +10,18 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     meta = models.CharField(max_length=300)
     content = models.TextField()
-    thumbnail_img = models.ImageField(null=True, blank=True, upload_to="images/")
-    thumbnail_url = models.URLField(blank=True, null=True)
+    
+    # This is the correct field for the uploaded image
+    thumbnail_img = CloudinaryField('image', folder='thlalak_portfolio/', null=True, blank=True)
+
+    # 1. ADD THIS PROPERTY to match the template variable {{blog.thumbnail_url}}
+    @property
+    def thumbnail_url(self):
+        # Check if the thumbnail_img field has a file/URL and return its public URL
+        if self.thumbnail_img and hasattr(self.thumbnail_img, 'url'):
+            return self.thumbnail_img.url
+        return "" # Return empty string if no image is present
+    
     category = models.CharField(max_length=255, default="uncategorized")
     slug = models.CharField(max_length=100)
     time = models.DateField(auto_now_add=True)
