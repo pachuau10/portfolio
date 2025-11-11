0912bf84e0a02619e9f957452c5ce6ac08233474
pachuau10 <duhteapachuau10@gmail.com>
2025-11-11 16:18:52 +0530

    admin page content fix

--- home/admin.py
+++ home/admin.py
@@ -4,7 +4,7 @@ from home.models import Blog
 
 # Register your models here.
 class BlogAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'id': "richtext_field"}))
+    
 
     class Meta:
         model = Blog
